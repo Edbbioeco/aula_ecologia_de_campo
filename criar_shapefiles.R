08d84bb9f3fa4ee90c0cb85707782ec2d2f66833
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-15 21:10:10 -0300

    Baixar o shapefile do PEDI

--- criar_shapefiles.R
+++ criar_shapefiles.R
@@ -11,3 +11,12 @@ library(leaflet)
 library(leaflet.extras)
 
 library(leafem)
+
+# Parque Estadual dois Irmãos ----
+
+## Baixar ----
+
+pedi <- geobr::read_conservation_units() |>
+  sf::st_make_valid() |>
+  dplyr::filter(name_conservation_unit |> stringr::str_detect("DOIS IRMÃOS") &
+                  category == "Parque")
