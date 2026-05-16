75c3223aef7aec20aa0d17d2a4f81545958b8bbb
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-16 12:55:32 -0300

    Importar e visualizar o shapefile do PEDI

--- baixar_imagem_de_satelite.R
+++ baixar_imagem_de_satelite.R
@@ -9,3 +9,18 @@ library(CDSE)
 library(terra)
 
 library(tidyterra)
+
+# Shapefiles ----
+
+## Shapefile do Parque Estadual Dois Irmãos ----
+
+### Importar ----
+
+pedi <- sf::st_read("pedi.shp")
+
+### Visualizar ----
+
+pedi
+
+ggplot() +
+  geom_sf(data = pedi, color = "black")
