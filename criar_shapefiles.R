77221030bd43adcbfd38e06030e59fc9da7efb07
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-15 21:29:05 -0300

    Criar e visuslizar o shapefile de referência para encontrar a UFPE

--- criar_shapefiles.R
+++ criar_shapefiles.R
@@ -32,3 +32,17 @@ ggplot() +
 
 pedi |>
   sf::st_write("pedi.shp")
+
+# UFPE ----
+
+## Shapefile de coordenada de referência ----
+
+sf_ref <- tibble::tibble(lon = "34°56'46\'W" |> parzer::parse_lon(),
+                         lat = "08°20'20\"S" |> parzer::parse_lat()) |>
+  sf::st_as_sf(coords = c("lon", "lat"),
+               crs = 4674)
+
+sf_ref
+
+ggplot() +
+  geom_sf(data = sf_ref, color = "black")
