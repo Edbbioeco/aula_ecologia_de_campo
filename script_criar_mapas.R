b4c3b40f366da0127a2082bef5585070c3b4c7e3
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-17 12:14:23 -0300

    Importar e visualizar o sahpefile do Brasil

--- script_criar_mapas.R
+++ script_criar_mapas.R
@@ -15,3 +15,18 @@ library(ggspatial)
 library(ggview)
 
 library(patchwork)
+
+# Dados ----
+
+## Brasil ----
+
+### Importar ----
+
+br <- sf::st_read("shapefiles/brasil/br.shp")
+
+### Visualizar ----
+
+br
+
+ggplot() +
+  geom_sf(data = br)
