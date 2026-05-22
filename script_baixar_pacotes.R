04f7ba93731cf234262ed84e5bceacd3ecb62eb0
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-21 21:40:42 -0300

    Script para baixar pacotes

---
+++ script_baixar_pacotes.R
@@ -0,0 +1,22 @@
+# Baixar pacotes ----
+
+# Rodar apenas uma vez!
+
+install.packages(c("sf",
+                   "tidyverse",
+                   "terra",
+                   "tidyverse",
+                   "ggview",
+                   "ggspatial",
+                   "patchwork",
+                   "devtools",
+                   "readxl",
+                   "vegan",
+                   "iNEXT",
+                   "scales",
+                   "betapart",
+                   "reshape2"))
+
+devtools::install_github("hughjonesd/ggmagnify")
+
+devtools::install_github("Edbbioeco/ordenaR")
