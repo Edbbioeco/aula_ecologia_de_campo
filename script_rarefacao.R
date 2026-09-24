633425eb8177b38278506d76ad10ab4439531342
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-24 00:13:16 -0300

    Condição para instalar pacotes

--- script_rarefacao.R
+++ script_rarefacao.R
@@ -1,3 +1,22 @@
+# Baixar Pacotes ----
+
+pacotes <- require(c("readxl",
+                     "tidyverse",
+                     "vegan",
+                     "iNEXT"))
+
+lapply(pacotes,
+       \(pacote){
+
+         if(!requireNamespace(pacotes, quietly = TRUE)){
+
+           install.packages(pacotes)
+
+         }
+
+       }
+)
+
 # Carregando os pacotes ----
 
 library(readxl)
